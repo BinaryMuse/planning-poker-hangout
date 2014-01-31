fb05fc536caca1725d5ee09115824b398cce4f16
Brandon Tilley <brandon@brandontilley.com>
2014-01-31 15:56:48 -0800

    Add app boot test

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 var app = angular.module('planning-poker', []);
 
+app.run(function($log) {
+  $log.debug('planning poker started');
+});
+
 var root = document.getElementById('app-root');
 angular.bootstrap(root, ['planning-poker']);
